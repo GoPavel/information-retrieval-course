1f74df26dcfa4fbdd60ad637a3db801488458166
Golovin Pavel <gopavel0@gmail.com>
2020-04-26 15:25:30 +0300

    add interface, README, fix gitignore

---
+++ antispam/README.md
@@ -0,0 +1,16 @@
+### build
+
+```bash
+$ poetry install
+$ poetry shell
+
+$ python main.py --help                                    
+usage: main.py [-h] --train TRAIN --test TEST
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --train TRAIN  path to train
+  --test TEST    path to test
+
+
+```
--- antispam/main.py
+++ antispam/main.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 import gzip
 from collections import namedtuple
@@ -39,42 +40,49 @@ def load_csv(input_file_name, calc_features_f, total: int = None):
         # trace(i, 1)
 
 
-def main():
+def main(args):
+    test_path = args.test
+    train_path = args.train
+
     get_html = lambda url, html_data: html_data
     skip_feature = lambda url, html_data: None
     vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-    test = load_csv('data/kaggle_test_data_tab.csv.gz', get_html, total=16039)
+    test = load_csv(test_path, get_html, total=16039)
     vectorizer.fit([item.features for item in test])
-    train = load_csv('data/kaggle_train_data_tab.csv.gz', get_html, total=7044)
+    train = load_csv(train_path, get_html, total=7044)
     X = vectorizer.fit_transform([item.features for item in train])
     print(">>> vectorizer fitted <<<")
 
     # get_url = lambda url, html_data: url
     # vectorizer_url = TfidfVectorizer(ngram_range=(5, 10), analyzer='char')
-    # test = load_csv('data/kaggle_test_data_tab.csv.gz', get_url, total=16039)
+    # test = load_csv(test_path, get_url, total=16039)
     # vectorizer_url.fit([item.features for item in test])
-    # train = load_csv('data/kaggle_train_data_tab.csv.gz', get_url, total=7044)
+    # train = load_csv(train_path, get_url, total=7044)
     # X_url = vectorizer_url.fit_transform([item.features for item in train])
     # print(">>> url vectorizer fitted <<<")
 
     clf = SGDClassifier(max_iter=1000, tol=1e-4)
-    train = load_csv('data/kaggle_train_data_tab.csv.gz', skip_feature, total=7044)
+    train = load_csv(train_path, skip_feature, total=7044)
     # clf.fit(scipy.sparse.hstack((X, X_url), format='csr'), np.array([item.is_spam for item in train]))
     clf.fit(X, np.array([item.is_spam for item in train]))
     print('>>> classifier fitted <<<')
 
-    test = load_csv('data/kaggle_test_data_tab.csv.gz', get_html, total=16039)
+    test = load_csv(test_path, get_html, total=16039)
     X_test = vectorizer.transform([item.features for item in test])
-    # test = load_csv('data/kaggle_test_data_tab.csv.gz', get_url, total=16039)
+    # test = load_csv(test_path, get_url, total=16039)
     # X_url_test = vectorizer_url.transform([item.features for item in test])
     # X_test = scipy.sparse.hstack((X_test, X_url_test), format='csr')
 
     with open('submission.txt', 'w') as f:
         f.write('Id,Prediction\n')
-        test = load_csv('data/kaggle_test_data_tab.csv.gz', skip_feature, total=16039)
+        test = load_csv(test_path, skip_feature, total=16039)
         for i, item in enumerate(test):
             f.write(f'{item.doc_id},{clf.predict(X_test[i])[0]:b}\n')
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', type=str, help="path to train", required=True)
+    parser.add_argument('--test', type=str, help='path to test', required=True)
+
+    main(parser.parse_args())
